4af7c36c085e5d944a588b45316cbff367bfc543
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-09-15 16:04:54 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 import Layout from './components/layout/Layout.tsx';
-import { UserProvider } from './components/context/UserContext';
-import { PostProvider } from './components/context/PostContext';
-import { ModalAddPostProvider } from './components/context/ModalAddPostContext';
+import { UserProvider } from './components/context/UserContext.tsx';
+import { PostProvider } from './components/context/PostContext.tsx';
+import { ModalAddPostProvider } from './components/context/ModalAddPostContext.tsx';
 import Homepage from './components/homepage/Homepage.tsx';
 import ModalAddPost from './components/modals/ModalAddPost.tsx';
 
