0fbe5456835c53ccddcc8853c9353f2d4f165ed6
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-09-15 17:20:52 +0300

    Update userContext.tsx

--- src/components/context/userContext.tsx
+++ src/components/context/userContext.tsx
@@ -3,7 +3,7 @@ import { fetchUsers } from '../../api/fetchUser.ts';
 import { type User } from '../../types/types.ts';
 
 type UserContextType = {
-    users: User[] | null,
+    users: User[] | [],
     userLoading: boolean,
     refreshUsers: VoidFunction,
 };
